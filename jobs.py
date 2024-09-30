8f18cc0bc20973e257226a7a3fdbf5e3837f0839
elchanan <elchanan.vol@gmail.com>
2024-09-30 23:41:59 +0300

    add logs

--- jobs.py
+++ jobs.py
@@ -1,12 +1,14 @@
-import pandas as pd
+import logging
+import traceback
+import os
 from datetime import date
-from ai import *
-from jobs_scraper import *
+import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
-import logging
-import os
-logging.basicConfig(level=logging.INFO)
+from ai import *
+from jobs_scraper import *
+
+
 
 excel_file = "jobs.xlsx"
 required_columns = ["applied", "AI_recommendation", "AI_explanation", "company", "title", "link", "description",
@@ -15,6 +17,7 @@ required_columns = ["applied", "AI_recommendation", "AI_explanation", "company",
 pd.set_option('display.max_rows', None)  # Show all rows
 pd.set_option('display.max_columns', None)  # Show all columns
 pd.set_option('display.width', None)
+logging.basicConfig(level=logging.INFO)
 
 
 def load_env_file(file_path):
@@ -43,6 +46,8 @@ def split_string(input_string):
     for word in starting_words:
         if input_string.startswith(word):
             return word, input_string[len(word):].strip()
+    logging.error(f"Invalid answer of AI: doesnt start with {starting_words}")
+    return "", input_string
 
 
 def beautify_excel():
@@ -72,54 +77,65 @@ def beautify_excel():
 
 
 def general_scrape_and_ai(unique_urls, assistant):
-    new_data = [0]
-    new_df = pd.DataFrame(columns=required_columns)
+    """
+    scrape and filter.
+    The script runs in a loop until all data is scraped (to handle cases where the process may be blocked midway,
+    such as by LinkedIn). By default, there is a break statement in the loop, which stops the process after a single
+    attempt. If you want the process to continue scraping until all data is collected, you should remove the break
+    statement. Keep in mind, if the program fails midway, there is a risk of data loss.
+    :param unique_urls: set of all urls already scraped and filtered
+    :param assistant: AI assistant
+    :return: df with all new scraped job including the AI filter
+    """
     offset = 0
-    while len(
-            new_data) > 0:  # running in loop until scraping all data (in case that it blocked in middle of scraping by linkdein etc)
+    new_data = [0]
+    df = pd.DataFrame(columns=required_columns)
+
+    while len(new_data) > 0:
         try:
-            print(f"offset:  {offset}")
+            logging.info(f"Offset of scraping is: {offset}")
             new_data = scrape_all_jobs(os.getenv('sites'), os.getenv('search_term'), os.getenv('location'),
                                        os.getenv('hours_old'),
                                        os.getenv('results_wanted'), offset)
-
-            print(f"len(new_data): {len(new_data)}")
+            logging.info(f"{len(new_data)} jobs scraped ")
             # new_data.to_csv(f"temp{offset}.csv", index=False)
             # new_data = pd.read_csv("temp.csv")
         except Exception as e:
-            print(f"Error while scraping data: ,{str(e)}")
+            logging.error("An error occurred while scraping: %s", e)
+            logging.error("Stack trace: %s", traceback.format_exc())
             new_data = pd.DataFrame(columns=required_columns)
+
         for index, row in new_data.iterrows():
-            if row['job_url'] not in unique_urls:
-                try:
-                    msg = f"title: {row['title']}. description: {row['description']}"
+            try:
+                if row['job_url'] in unique_urls:
+                    continue
+                msg = f"title: {row['title']}. description: {row['description']}"
+                ai_response = assistant.submit_message(msg)
+                # print(ai_recommend)
+                recommendation, explanation = split_string(ai_response)
+                new_row = {
+                    "applied": "",
+                    "AI_recommendation": recommendation,
+                    "AI_explanation": explanation,
+                    "company": row['company'],
+                    "title": row['title'],
+                    "link": row['job_url'],
+                    "description": row['description'],
+                    "posted_date": row['date_posted'],
+                    "scrape_date": date.today(),
 
-                    ai_recommend = assistant.submit_message(msg)
-                    # print(ai_recommend)
-                    recommendation, explanation = split_string(ai_recommend)
-                    new_row = {
-                        "applied": "",
-                        "AI_recommendation": recommendation,
-                        "AI_explanation": explanation,
-
-                        "company": row['company'],
-                        "title": row['title'],
-                        "link": row['job_url'],
-                        "description": row['description'],
-                        "posted_date": row['date_posted'],
-                        "scrape_date": date.today(),
-
-                    }
-                    new_df.loc[len(new_df)] = new_row
-                    # new_row_df = pd.DataFrame([new_row])
-                    # new_row_df.to_csv(f"after_ai_temp.csv", mode='a', index=False, header=False)
-                    unique_urls.add(new_row['link'])
-                except Exception as e:
-                    print(f"Error while sending to gpt: ,{str(e)}")
+                }
+                df.loc[len(df)] = new_row
+                # new_row_df = pd.DataFrame([new_row])
+                # new_row_df.to_csv(f"after_ai_temp.csv", mode='a', index=False, header=False)
+                unique_urls.add(new_row['link'])
+            except Exception as e:
+                logging.error("An error occurred while sending to gpt: %s", e)
+                logging.error("Stack trace: %s", traceback.format_exc())
         offset += len(new_data)
-        break  # stop the loop
+        break
 
-    return new_df
+    return df
 
 
 def main():
@@ -127,11 +143,12 @@ def main():
     data = load_df()
     unique_urls = set(data["link"])
     assistant_name = "MyJobsMatcher"
-    with open('instructions.txt', 'r',encoding='utf-8') as file:
+    with open('instructions.txt', 'r', encoding='utf-8') as file:
         instructions = file.read()
     assistant = OpenAIAssistant(os.getenv('api_key'), assistant_name, instructions, os.getenv('model'),
                                 os.getenv('assistant_id'))
-    print(assistant.assistant_id) # for using the same assistant for the next round
+    # for using the same assistant for the next round
+    logging.info(f"Assistant created successfully. assistant ID: {assistant.assistant_id}")
     new_df = general_scrape_and_ai(unique_urls, assistant)
     df = pd.concat([data, new_df])
     df.to_excel(excel_file, index=False, engine='openpyxl')
